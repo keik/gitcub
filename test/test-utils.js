d5540a5acb80f37e450d6f6bd4e9078e91538372
keik <k4t0.kei@gmail.com>
2016-10-25 08:03:58 +0900

    Implement file tree view

--- test/test-utils.js
+++ test/test-utils.js
@@ -48,20 +48,17 @@ test('parse entries by directories', (t) => {
     }
   }
 
-  const parsed0 = utils.parseEntriesByDirLevel(entries, 0)
-  const parsed1 = utils.parseEntriesByDirLevel(entries, 1)
-  const parsed2 = utils.parseEntriesByDirLevel(entries, 2)
-  const parsed3 = utils.parseEntriesByDirLevel(entries, 3)
-  t.deepEqual(parsed0, { file1: { lastCommit: { message: 'Add file1\n', date: '2016-09-24T05:48:16.000Z' }, isFile: true },
+  const parsed_ = utils.parseEntriesByDirLevel(entries, '')
+  const parsed_codes = utils.parseEntriesByDirLevel(entries, 'codes')
+  const parsed_codes_d = utils.parseEntriesByDirLevel(entries, 'codes/d')
+  t.deepEqual(parsed_, { file1: { lastCommit: { message: 'Add file1\n', date: '2016-09-24T05:48:16.000Z' }, isFile: true },
                          file2: { lastCommit: { message: 'Add `!` to file2\n', date: '2016-09-27T01:08:48.000Z' }, isFile: true },
                          file3: { lastCommit: { message: 'Add file3\n', date: '2016-09-27T01:08:08.000Z' }, isFile: true },
                          codes: { lastCommit: { message: 'Add codes3\n', date: '2016-09-27T02:43:37.000Z' }, isFile: false },
                          d: { lastCommit: { message: 'Add d/file3\n', date: '2016-09-25T04:15:47.000Z' }, isFile: false } })
-  t.deepEqual(parsed1, { 'file.js': { lastCommit: { message: 'Add codes1\n', date: '2016-09-27T02:41:37.000Z' }, isFile: true },
-                         d: { lastCommit: { message: 'Add codes3\n', date: '2016-09-27T02:43:37.000Z' }, isFile: false },
-                         'file.rb': { lastCommit: { message: 'Add codes3\n', date: '2016-09-27T02:42:37.000Z' }, isFile: true },
-                         file3: { lastCommit: { message: 'Add d/file3\n', date: '2016-09-25T04:15:47.000Z' }, isFile: true } })
+  t.deepEqual(parsed_codes, { 'file.js': { lastCommit: { message: 'Add codes1\n', date: '2016-09-27T02:41:37.000Z' }, isFile: true },
+                              d: { lastCommit: { message: 'Add codes3\n', date: '2016-09-27T02:43:37.000Z' }, isFile: false },
+                              'file.rb': { lastCommit: { message: 'Add codes3\n', date: '2016-09-27T02:42:37.000Z' }, isFile: true }})
 
-  t.deepEqual(parsed2, { 'file.md': { lastCommit: { message: 'Add codes3\n', date: '2016-09-27T02:43:37.000Z' }, isFile: true } })
-  t.deepEqual(parsed3, {})
+  t.deepEqual(parsed_codes_d, { 'file.md': { lastCommit: { message: 'Add codes3\n', date: '2016-09-27T02:43:37.000Z' }, isFile: true } })
 })
