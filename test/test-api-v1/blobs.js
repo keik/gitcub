e26151b0b3750f9aff938279f79e0c743dc45d31
keik <k4t0.kei@gmail.com>
2016-10-26 08:19:20 +0900

    Organize tests name

--- test/test-api-v1/blobs.js
+++ test/test-api-v1/blobs.js
@@ -4,7 +4,16 @@ import axios from 'axios'
 import { API_BLOBS } from '../../lib/server/routers/api/v1'
 
 export default function(config) {
-  test.cb(`GET ${API_BLOBS} should return 200 and file info`, (t) => {
+  test.cb(`GET ${API_BLOBS} with no parameter should return 404`, (t) => {
+    axios.get(`http://localhost:${config.PORT}/api/v1/repos/user1/repo1/git/blobs`)
+      .then((res) => {
+        t.fail(res.toString())
+      }).catch((err) => {
+        t.is(err.response.status, 404)
+      }).finally(t.end)
+  })
+
+  test.cb(`GET ${API_BLOBS} with SHA should return file info`, (t) => {
     axios.get(`http://localhost:${config.PORT}/api/v1/repos/user1/repo1/git/blobs/b97038f29f6d581aa86d6417f9ed464c1cdfeba2`)
       .then((res) => {
         t.deepEqual(res.data,
@@ -16,7 +25,7 @@ export default function(config) {
       }).finally(t.end)
   })
 
-  test.cb(`GET ${API_BLOBS} should return 200 and file info`, (t) => {
+  test.cb(`GET ${API_BLOBS} with REFERENCE/PATH should return file info`, (t) => {
     axios.get(`http://localhost:${config.PORT}/api/v1/repos/user1/repo1/git/blobs/master/file1`)
       .then((res) => {
         t.deepEqual(res.data,
@@ -28,7 +37,7 @@ export default function(config) {
       }).finally(t.end)
   })
 
-  test.cb(`GET ${API_BLOBS} should return 200 and file info`, (t) => {
+  test.cb(`GET ${API_BLOBS} with REFERENCE/NESTED_PATH should return file info`, (t) => {
     axios.get(`http://localhost:${config.PORT}/api/v1/repos/user1/repo1/git/blobs/master/d/dd/nested`)
       .then((res) => {
         t.deepEqual(res.data,
@@ -40,7 +49,7 @@ export default function(config) {
       }).finally(t.end)
   })
 
-  test.cb(`GET ${API_BLOBS} should return 200 and file info`, (t) => {
+  test.cb(`GET ${API_BLOBS} with SHA/PATH should return file info`, (t) => {
     axios.get(`http://localhost:${config.PORT}/api/v1/repos/user1/repo1/git/blobs/f1582566910f7a5d41b47f0c93ed560e1e1fd8d0/file1`)
       .then((res) => {
         t.deepEqual(res.data,
--- test/test-api-v1/trees.js
+++ test/test-api-v1/trees.js
@@ -4,16 +4,7 @@ import axios from 'axios'
 import { API_TAGS } from '../../lib/server/routers/api/v1'
 
 export default function(config) {
-  test.cb(`GET ${API_TAGS} should return 200 and name of entries (sha)`, (t) => {
-    axios.get(`http://localhost:${config.PORT}/api/v1/repos/user1/repo1/git/trees/297862f2168af863ae0e2735caabe8b7461f188f`)
-      .then(({data: result}) => {
-        t.is(result.tree.length, 4)
-        t.deepEqual(Object.keys(result.tree[0]), ['path', 'type', 'sha'])
-      }).catch((err) => {
-        t.fail(err.toString())
-      }).finally(t.end)
-  })
-  test.cb(`GET ${API_TAGS} should return 200 and name of entries (default branch)`, (t) => {
+  test.cb(`GET ${API_TAGS} with no parameter should return name of entries in default branch`, (t) => {
     axios.get(`http://localhost:${config.PORT}/api/v1/repos/user1/repo1/git/trees`)
       .then(({data: result}) => {
         t.is(result.tree.length, 8)
@@ -22,7 +13,18 @@ export default function(config) {
         t.fail(err.toString())
       }).finally(t.end)
   })
-  test.cb(`GET ${API_TAGS} should return 200 and name of entries (feature branch)`, (t) => {
+
+  test.cb(`GET ${API_TAGS} with SHA should return name of entries`, (t) => {
+    axios.get(`http://localhost:${config.PORT}/api/v1/repos/user1/repo1/git/trees/297862f2168af863ae0e2735caabe8b7461f188f`)
+      .then(({data: result}) => {
+        t.is(result.tree.length, 4)
+        t.deepEqual(Object.keys(result.tree[0]), ['path', 'type', 'sha'])
+      }).catch((err) => {
+        t.fail(err.toString())
+      }).finally(t.end)
+  })
+
+  test.cb(`GET ${API_TAGS} with REFERENCE should return name of entries`, (t) => {
     axios.get(`http://localhost:${config.PORT}/api/v1/repos/user1/repo1/git/trees/feature`)
       .then(({data: result}) => {
         t.is(result.tree.length, 4)
@@ -31,7 +33,8 @@ export default function(config) {
         t.fail(err.toString())
       }).finally(t.end)
   })
-  test.cb(`GET ${API_TAGS} should return 200 and name of entries (with last_commit)`, (t) => {
+
+  test.cb(`GET ${API_TAGS} with REFERENCE?last_commit should return name of entries with last_commit`, (t) => {
     axios.get(`http://localhost:${config.PORT}/api/v1/repos/user1/repo1/git/trees/297862f2168af863ae0e2735caabe8b7461f188f?last_commit=1`)
       .then(({data: result}) => {
         t.is(result.tree.length, 4)
