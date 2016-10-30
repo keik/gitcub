10de512bb10a1a88b24c72c69d21c202d94648f5
keik <k4t0.kei@gmail.com>
2016-10-30 13:25:36 +0900

    Add last commit anchor to entries view

--- test/test-api-v1/git-trees.js
+++ test/test-api-v1/git-trees.js
@@ -105,19 +105,85 @@ export default function(config) {
             [ { path: 'd/file3',
               type: 'blob',
               sha: 'd2cebd4f0a9e97a48a6139d09cafdb513ad8fee3',
-              lastCommit: { message: 'Add d/file3\n', date: '2016-09-25T04:15:47.000Z' } },
+              lastCommit:
+              { author:
+                { date: '2016-09-25T04:15:47.000Z',
+                  email: 'k4t0.kei@gmail.com',
+                  name: 'keik' },
+                committer:
+                { date: '2016-09-25T04:15:47.000Z',
+                  email: 'k4t0.kei@gmail.com',
+                  name: 'keik' },
+                message: 'Add d/file3\n',
+                parents:
+                [ { url: '/api/v1/repos/repo1/user1/git/commits/3c27dc60c76be4a1f5b765cb141d0d22d871a2b6',
+                  sha: '3c27dc60c76be4a1f5b765cb141d0d22d871a2b6' } ],
+                sha: 'e801527fecc2efb3a2e710a21a226ca0abf9db63',
+                tree:
+                { url: '/api/v1/repos/repo1/user1/git/trees(/20bd2419ce4536e4e62700550d23ba7c3233eed9)?',
+                  sha: '20bd2419ce4536e4e62700550d23ba7c3233eed9' },
+                url: '/git/commits/e801527fecc2efb3a2e710a21a226ca0abf9db63' } },
               { path: 'file1',
                 type: 'blob',
                 sha: 'ce013625030ba8dba906f756967f9e9ca394464a',
-                lastCommit: { message: 'Add file1\n', date: '2016-09-24T05:48:16.000Z' } },
+                lastCommit:
+                { author:
+                  { date: '2016-09-24T05:48:16.000Z',
+                    email: 'k4t0.kei@gmail.com',
+                    name: 'keik' },
+                  committer:
+                  { date: '2016-09-24T05:48:16.000Z',
+                    email: 'k4t0.kei@gmail.com',
+                    name: 'keik' },
+                  message: 'Add file1\n',
+                  parents: [],
+                  sha: 'd29e783434a7fadfa5bbf7b361dfc20a83ad8722',
+                  tree:
+                  { url: '/api/v1/repos/repo1/user1/git/trees(/d001d287018593691c36042e1c8089fde7415296)?',
+                    sha: 'd001d287018593691c36042e1c8089fde7415296' },
+                  url: '/git/commits/d29e783434a7fadfa5bbf7b361dfc20a83ad8722' } },
               { path: 'file2',
                 type: 'blob',
                 sha: 'cc628ccd10742baea8241c5924df992b5c019f71',
-                lastCommit: { message: 'Add file2\n', date: '2016-09-24T05:48:36.000Z' } },
+                lastCommit:
+                { author:
+                  { date: '2016-09-24T05:48:36.000Z',
+                    email: 'k4t0.kei@gmail.com',
+                    name: 'keik' },
+                  committer:
+                  { date: '2016-09-24T05:48:36.000Z',
+                    email: 'k4t0.kei@gmail.com',
+                    name: 'keik' },
+                  message: 'Add file2\n',
+                  parents:
+                  [ { url: '/api/v1/repos/repo1/user1/git/commits/d29e783434a7fadfa5bbf7b361dfc20a83ad8722',
+                    sha: 'd29e783434a7fadfa5bbf7b361dfc20a83ad8722' } ],
+                  sha: '3c27dc60c76be4a1f5b765cb141d0d22d871a2b6',
+                  tree:
+                  { url: '/api/v1/repos/repo1/user1/git/trees(/812bcf7a7db574cf24a2d6b8ed92cfd096c219e5)?',
+                    sha: '812bcf7a7db574cf24a2d6b8ed92cfd096c219e5' },
+                  url: '/git/commits/3c27dc60c76be4a1f5b765cb141d0d22d871a2b6' } },
               { path: 'file3',
                 type: 'blob',
                 sha: '57651f675c8e781cf377630612076c16b83fe780',
-                lastCommit: { message: 'Add file3\n', date: '2016-09-27T01:08:08.000Z' } } ] }
+                lastCommit:
+                { author:
+                  { date: '2016-09-26T15:07:53.000Z',
+                    email: 'k4t0.kei@gmail.com',
+                    name: 'keik' },
+                  committer:
+                  { date: '2016-09-27T01:08:08.000Z',
+                    email: 'k4t0.kei@gmail.com',
+                    name: 'keik' },
+                  message: 'Add file3\n',
+                  parents:
+                  [ { url: '/api/v1/repos/repo1/user1/git/commits/e801527fecc2efb3a2e710a21a226ca0abf9db63',
+                    sha: 'e801527fecc2efb3a2e710a21a226ca0abf9db63' } ],
+                  sha: '297862f2168af863ae0e2735caabe8b7461f188f',
+                  tree:
+                  { url: '/api/v1/repos/repo1/user1/git/trees(/6be2dafb160ab380d886f5a9a488843653f4aaa3)?',
+                    sha: '6be2dafb160ab380d886f5a9a488843653f4aaa3' },
+                  url: '/git/commits/297862f2168af863ae0e2735caabe8b7461f188f' } } ] }
         )
       }).catch((err) => {
         t.fail(err.toString())
