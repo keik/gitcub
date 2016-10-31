8d37c5ffb0ea5a840c91bb452e862701256b7849
keik <k4t0.kei@gmail.com>
2016-10-31 22:02:28 +0900

    Refine repos-commit API

--- test/test-api-v1/git-commits.js
+++ test/test-api-v1/git-commits.js
@@ -7,24 +7,25 @@ export default function(config) {
   test.cb(`GET ${API_GIT_COMMITS} with SHA should return a specified commit`, (t) => {
     axios.get(`http://localhost:${config.PORT}/api/v1/repos/user1/repo1/git/commits/0100c14d9341db683c43e47c6944ecb1616005bd`)
       .then((res) => {
+        // console.log(require('util').inspect(res.data, false, null))
         t.deepEqual(res.data,
           { author:
-            { date: '2016-10-24T14:13:53.000Z',
+            { name: 'keik',
               email: 'k4t0.kei@gmail.com',
-              name: 'keik' },
+              date: '2016-10-24T14:13:53.000Z' },
             committer:
-            { date: '2016-10-24T14:13:53.000Z',
+            { name: 'keik',
               email: 'k4t0.kei@gmail.com',
-              name: 'keik' },
+              date: '2016-10-24T14:13:53.000Z' },
             message: 'Add nested file\n',
             parents:
-            [ { url: '/api/v1/repos/user1/repo1/git/commits/f1582566910f7a5d41b47f0c93ed560e1e1fd8d0',
+            [ { url: 'http://localhost:3000/api/v1/repos/user1/repo1/git/commits/f1582566910f7a5d41b47f0c93ed560e1e1fd8d0',
               sha: 'f1582566910f7a5d41b47f0c93ed560e1e1fd8d0' } ],
             sha: '0100c14d9341db683c43e47c6944ecb1616005bd',
             tree:
-            { url: '/api/v1/repos/user1/repo1/git/trees(/dfd076cc884b4554bab928f62123fb262c02ec6c)?',
+            { url: 'http://localhost:3000/api/v1/repos/user1/repo1/git/trees/dfd076cc884b4554bab928f62123fb262c02ec6c',
               sha: 'dfd076cc884b4554bab928f62123fb262c02ec6c' },
-            url: '/git/commits/0100c14d9341db683c43e47c6944ecb1616005bd' }
+            url: 'http://localhost:3000/api/v1/repos/user1/repo1/git/commits/0100c14d9341db683c43e47c6944ecb1616005bd' }
         )
       })
       .catch((err) => {
--- test/test-api-v1/git-trees.js
+++ test/test-api-v1/git-trees.js
@@ -98,6 +98,7 @@ export default function(config) {
     axios.get(`http://localhost:${config.PORT}/api/v1/repos/user1/repo1/git/trees/297862f2168af863ae0e2735caabe8b7461f188f?last_commit=1`)
       .then((res) => {
         res.data.tree.sort((a, b) => a.path > b.path)
+        console.log(require('util').inspect(res.data, false, null))
         t.deepEqual(res.data,
           { url: '/api/v1/repos/user1/repo1/git/trees/297862f2168af863ae0e2735caabe8b7461f188f?last_commit=1',
             sha: '297862f2168af863ae0e2735caabe8b7461f188f',
@@ -107,84 +108,84 @@ export default function(config) {
               sha: 'd2cebd4f0a9e97a48a6139d09cafdb513ad8fee3',
               lastCommit:
               { author:
-                { date: '2016-09-25T04:15:47.000Z',
+                { name: 'keik',
                   email: 'k4t0.kei@gmail.com',
-                  name: 'keik' },
+                  date: '2016-09-25T04:15:47.000Z' },
                 committer:
-                { date: '2016-09-25T04:15:47.000Z',
+                { name: 'keik',
                   email: 'k4t0.kei@gmail.com',
-                  name: 'keik' },
+                  date: '2016-09-25T04:15:47.000Z' },
                 message: 'Add d/file3\n',
                 parents:
-                [ { url: '/api/v1/repos/repo1/user1/git/commits/3c27dc60c76be4a1f5b765cb141d0d22d871a2b6',
+                [ { url: 'http://localhost:3000/api/v1/repos/repo1/user1/git/commits/3c27dc60c76be4a1f5b765cb141d0d22d871a2b6',
                   sha: '3c27dc60c76be4a1f5b765cb141d0d22d871a2b6' } ],
                 sha: 'e801527fecc2efb3a2e710a21a226ca0abf9db63',
                 tree:
-                { url: '/api/v1/repos/repo1/user1/git/trees(/20bd2419ce4536e4e62700550d23ba7c3233eed9)?',
+                { url: 'http://localhost:3000/api/v1/repos/repo1/user1/git/trees/20bd2419ce4536e4e62700550d23ba7c3233eed9',
                   sha: '20bd2419ce4536e4e62700550d23ba7c3233eed9' },
-                url: '/git/commits/e801527fecc2efb3a2e710a21a226ca0abf9db63' } },
+                url: 'http://localhost:3000/api/v1/repos/repo1/user1/git/commits/e801527fecc2efb3a2e710a21a226ca0abf9db63' } },
               { path: 'file1',
                 type: 'blob',
                 sha: 'ce013625030ba8dba906f756967f9e9ca394464a',
                 lastCommit:
                 { author:
-                  { date: '2016-09-24T05:48:16.000Z',
+                  { name: 'keik',
                     email: 'k4t0.kei@gmail.com',
-                    name: 'keik' },
+                    date: '2016-09-24T05:48:16.000Z' },
                   committer:
-                  { date: '2016-09-24T05:48:16.000Z',
+                  { name: 'keik',
                     email: 'k4t0.kei@gmail.com',
-                    name: 'keik' },
+                    date: '2016-09-24T05:48:16.000Z' },
                   message: 'Add file1\n',
                   parents: [],
                   sha: 'd29e783434a7fadfa5bbf7b361dfc20a83ad8722',
                   tree:
-                  { url: '/api/v1/repos/repo1/user1/git/trees(/d001d287018593691c36042e1c8089fde7415296)?',
+                  { url: 'http://localhost:3000/api/v1/repos/repo1/user1/git/trees/d001d287018593691c36042e1c8089fde7415296',
                     sha: 'd001d287018593691c36042e1c8089fde7415296' },
-                  url: '/git/commits/d29e783434a7fadfa5bbf7b361dfc20a83ad8722' } },
+                  url: 'http://localhost:3000/api/v1/repos/repo1/user1/git/commits/d29e783434a7fadfa5bbf7b361dfc20a83ad8722' } },
               { path: 'file2',
                 type: 'blob',
                 sha: 'cc628ccd10742baea8241c5924df992b5c019f71',
                 lastCommit:
                 { author:
-                  { date: '2016-09-24T05:48:36.000Z',
+                  { name: 'keik',
                     email: 'k4t0.kei@gmail.com',
-                    name: 'keik' },
+                    date: '2016-09-24T05:48:36.000Z' },
                   committer:
-                  { date: '2016-09-24T05:48:36.000Z',
+                  { name: 'keik',
                     email: 'k4t0.kei@gmail.com',
-                    name: 'keik' },
+                    date: '2016-09-24T05:48:36.000Z' },
                   message: 'Add file2\n',
                   parents:
-                  [ { url: '/api/v1/repos/repo1/user1/git/commits/d29e783434a7fadfa5bbf7b361dfc20a83ad8722',
+                  [ { url: 'http://localhost:3000/api/v1/repos/repo1/user1/git/commits/d29e783434a7fadfa5bbf7b361dfc20a83ad8722',
                     sha: 'd29e783434a7fadfa5bbf7b361dfc20a83ad8722' } ],
                   sha: '3c27dc60c76be4a1f5b765cb141d0d22d871a2b6',
                   tree:
-                  { url: '/api/v1/repos/repo1/user1/git/trees(/812bcf7a7db574cf24a2d6b8ed92cfd096c219e5)?',
+                  { url: 'http://localhost:3000/api/v1/repos/repo1/user1/git/trees/812bcf7a7db574cf24a2d6b8ed92cfd096c219e5',
                     sha: '812bcf7a7db574cf24a2d6b8ed92cfd096c219e5' },
-                  url: '/git/commits/3c27dc60c76be4a1f5b765cb141d0d22d871a2b6' } },
+                  url: 'http://localhost:3000/api/v1/repos/repo1/user1/git/commits/3c27dc60c76be4a1f5b765cb141d0d22d871a2b6' } },
               { path: 'file3',
                 type: 'blob',
                 sha: '57651f675c8e781cf377630612076c16b83fe780',
                 lastCommit:
                 { author:
-                  { date: '2016-09-26T15:07:53.000Z',
+                  { name: 'keik',
                     email: 'k4t0.kei@gmail.com',
-                    name: 'keik' },
+                    date: '2016-09-26T15:07:53.000Z' },
                   committer:
-                  { date: '2016-09-27T01:08:08.000Z',
+                  { name: 'keik',
                     email: 'k4t0.kei@gmail.com',
-                    name: 'keik' },
+                    date: '2016-09-27T01:08:08.000Z' },
                   message: 'Add file3\n',
                   parents:
-                  [ { url: '/api/v1/repos/repo1/user1/git/commits/e801527fecc2efb3a2e710a21a226ca0abf9db63',
+                  [ { url: 'http://localhost:3000/api/v1/repos/repo1/user1/git/commits/e801527fecc2efb3a2e710a21a226ca0abf9db63',
                     sha: 'e801527fecc2efb3a2e710a21a226ca0abf9db63' } ],
                   sha: '297862f2168af863ae0e2735caabe8b7461f188f',
                   tree:
-                  { url: '/api/v1/repos/repo1/user1/git/trees(/6be2dafb160ab380d886f5a9a488843653f4aaa3)?',
+                  { url: 'http://localhost:3000/api/v1/repos/repo1/user1/git/trees/6be2dafb160ab380d886f5a9a488843653f4aaa3',
                     sha: '6be2dafb160ab380d886f5a9a488843653f4aaa3' },
-                  url: '/git/commits/297862f2168af863ae0e2735caabe8b7461f188f' } } ] }
-        )
+                  url: 'http://localhost:3000/api/v1/repos/repo1/user1/git/commits/297862f2168af863ae0e2735caabe8b7461f188f' } } ] }
+          )
       }).catch((err) => {
         t.fail(err.toString())
       }).finally(t.end)
