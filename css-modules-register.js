49a0c54f30b10ae0c6692ba5443fedf2449edb87
keik <k4t0.kei@gmail.com>
2016-11-02 01:30:47 +0900

    Change css modules scoped name rule

--- css-modules-register.js
+++ css-modules-register.js
@@ -1,3 +1,7 @@
+const cssModulesify = require('css-modulesify')
 require('css-modules-require-hook')({
-  rootDir: './lib/share'
+  rootDir: './lib/share',
+  generateScopedName: process.env.NODE_ENV === 'production' ?
+    cssModulesify.generateShortName :
+    undefined,
 })
