9e5c4ec4e840c30caa1f1360a4d163ea1b8142ff
keik <keik@c-fo.com>
2018-02-11 18:56:27 +0900

    :+1: Style user page

--- frontend/components/User.js
+++ frontend/components/User.js
@@ -1,101 +1,198 @@
 // @flow
 
 import * as React from 'react'
+import { GoLink, GoLocation, GoMail, GoOrganization } from 'react-icons/go'
 
 import InnerContainer from './common/layouts/InnerContainer'
 
 const User = () => (
   <InnerContainer>
     <div
-      css={{
+      css={({ theme: { spaces } }) => ({
         display: 'flex',
-        paddingRight: '16px'
-      }}
+        marginTop: spaces[2]
+      })}
     >
-      <div css={{ width: '25%' }}>
+      <div css={{ width: '25%', marginRight: '16px' }}>
         <div
           css={({ theme: { border, borderRadius } }) => ({
             border,
             borderRadius
           })}
         >
-          hoge
+          <img
+            alt="avator"
+            css={{
+              display: 'block',
+              width: '240px',
+              height: '240px',
+              backgroundColor: '#ccc'
+            }}
+          />
         </div>
-        <img
-          alt="avator"
-          style={{
-            width: 233,
-            height: 233,
-            borderRadius: 6,
-            backgroundColor: '#ccc'
-          }}
-        />
         <div>
-          <div>NAME</div>
-          <div>ID</div>
+          <h1>
+            <div
+              css={{
+                fontSize: '26px'
+              }}
+            >
+              %NAME%
+            </div>
+            <div
+              css={{
+                color: '#666666',
+                fontSize: '20px',
+                fontWeight: '300'
+              }}
+            >
+              %ID%
+            </div>
+          </h1>
           <div>
-            <ul style={{ listStyle: 'none', padding: 0, margin: 0 }}>
-              <li>PROF</li>
-              <li>PROF</li>
-              <li>PROF</li>
+            <div
+              css={({ theme: { spaces } }) => ({
+                marginTop: spaces[2]
+              })}
+            >
+              %BIO%
+            </div>
+            <ul
+              css={({ theme: { spaces } }) => ({
+                display: 'flex',
+                flexDirection: 'column',
+                listStyle: 'none',
+                padding: 0,
+                margin: `${spaces[2]} 0 0 0`,
+                '> li': {
+                  marginBottom: spaces[0]
+                }
+              })}
+            >
+              <li>
+                <GoOrganization />
+                %ORGANZATION%
+              </li>
+              <li>
+                <GoLocation />
+                %LOCATION%
+              </li>
+              <li>
+                <GoMail />
+                %EMAIL%
+              </li>
+              <li>
+                <GoLink />
+                %LINK%
+              </li>
             </ul>
+            <button>Edit</button>
           </div>
+          <hr />
           <div>
-            <ul style={{ listStyle: 'none', padding: 0, margin: 0 }}>
-              <li>
-                <img
-                  alt="ORG"
-                  style={{
-                    width: 35,
-                    height: 35,
-                    borderRadius: 3,
+            <h2 css={{ fontSize: '16px' }}>Organizations</h2>
+            <ul
+              css={({ theme: { borderRadius, spaces } }) => ({
+                display: 'flex',
+                listStyle: 'none',
+                padding: 0,
+                margin: 0,
+                '> li': {
+                  marginRight: spaces[0],
+                  '> img': {
+                    display: 'block',
+                    width: '36px',
+                    height: '36px',
+                    borderRadius: borderRadius,
                     backgroundColor: '#ccc'
-                  }}
-                />
+                  }
+                }
+              })}
+            >
+              <li>
+                <img alt="ORG" />
               </li>
               <li>
-                <img
-                  alt="ORG"
-                  style={{
-                    width: 35,
-                    height: 35,
-                    borderRadius: 3,
-                    backgroundColor: '#ccc'
-                  }}
-                />
+                <img alt="ORG" />
               </li>
               <li>
-                <img
-                  alt="ORG"
-                  style={{
-                    width: 35,
-                    height: 35,
-                    borderRadius: 3,
-                    backgroundColor: '#ccc'
-                  }}
-                />
+                <img alt="ORG" />
               </li>
             </ul>
           </div>
         </div>
       </div>
       <div css={{ width: '75%' }}>
-        <nav>
-          <ul style={{ listStyle: 'none', padding: 0, margin: 0 }}>
-            <li>Overview</li>
-            <li>Repositories</li>
-            <li>Stars</li>
-            <li>Followers</li>
-            <li>Following</li>
+        <nav css={({ theme: { border } }) => ({ borderBottom: border })}>
+          <ul
+            css={({ theme: { spaces } }) => ({
+              display: 'flex',
+              listStyle: 'none',
+              padding: 0,
+              margin: 0,
+              '> li': {
+                marginRight: spaces[2],
+                '> a': {
+                  display: 'block',
+                  padding: `${spaces[2]} ${spaces[1]}`
+                }
+              }
+            })}
+          >
+            <li>
+              <a href="#">Overview</a>
+            </li>
+            <li>
+              <a href="#">Repositories</a>
+            </li>
+            <li>
+              <a href="#">Stars</a>
+            </li>
+            <li>
+              <a href="#">Followers</a>
+            </li>
+            <li>
+              <a href="#">Following</a>
+            </li>
           </ul>
         </nav>
         <div>
           <h2>Pinned repositories</h2>
-          <ul style={{ listStyle: 'none', padding: 0, margin: 0 }}>
-            <li>REPO</li>
-            <li>REPO</li>
-            <li>REPO</li>
-            <li>REPO</li>
+          <ul
+            css={({ theme: { border, borderRadius } }) => ({
+              display: 'flex',
+              flexWrap: 'wrap',
+              justifyContent: 'space-between',
+              listStyle: 'none',
+              padding: 0,
+              margin: 0,
+              '> li': {
+                border,
+                borderRadius,
+                width: '49%' // WIP
+              }
+            })}
+          >
+            <li>
+              <h3>%REPO_NAME%</h3>
+              <p>%REPO_DESCRIPTION%</p>
+            </li>
+            <li>
+              <h3>%REPO_NAME%</h3>
+              <p>%REPO_DESCRIPTION%</p>
+            </li>
+            <li>
+              <h3>%REPO_NAME%</h3>
+              <p>%REPO_DESCRIPTION%</p>
+            </li>
+            <li>
+              <h3>%REPO_NAME%</h3>
+              <p>%REPO_DESCRIPTION%</p>
+            </li>
+            <li>
+              <h3>%REPO_NAME%</h3>
+              <p>%REPO_DESCRIPTION%</p>
+            </li>
           </ul>
         </div>
         <div>
--- frontend/theme.js
+++ frontend/theme.js
@@ -2,7 +2,8 @@
 
 const theme = {
   border: '1px solid #e1e4e8',
-  borderRadius: '3px'
+  borderRadius: '3px',
+  spaces: ['4px', '8px', '16px']
 }
 
 export default theme
