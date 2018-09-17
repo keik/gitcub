334d9377c8a6fe60392cab81d53ed288b40f84cb
keik <keik@c-fo.com>
2018-09-18 08:31:38 +0900

    :recycle: Config as JS

---
+++ config.js
@@ -0,0 +1,21 @@
+// @flow
+
+module.exports = {
+  env: {
+    production: {
+      HOST: 'localhost',
+      PORT: 3000,
+      REPO_ROOT: './test/fixture/repos'
+    },
+    development: {
+      HOST: 'localhost',
+      PORT: 3000,
+      REPO_ROOT: './test/fixture/repos'
+    },
+    test: {
+      HOST: 'localhost',
+      PORT: 3000,
+      REPO_ROOT: './test/fixture/repos'
+    }
+  }
+}
