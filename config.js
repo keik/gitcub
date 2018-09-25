55f0ebb37ce272d64dec4d6145cee47961ce609e
keik <keik@c-fo.com>
2018-09-25 10:59:42 +0900

    :shirt: Change default port

--- config.js
+++ config.js
@@ -4,17 +4,17 @@ module.exports = {
   env: {
     production: {
       HOST: 'localhost',
-      PORT: 3000,
+      PORT: 8000,
       REPO_ROOT: './test/fixture/repos'
     },
     development: {
       HOST: 'localhost',
-      PORT: 3000,
+      PORT: 8000,
       REPO_ROOT: './test/fixture/repos'
     },
     test: {
       HOST: 'localhost',
-      PORT: 3000,
+      PORT: 8000,
       REPO_ROOT: './test/fixture/repos'
     }
   }
