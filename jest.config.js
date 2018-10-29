a38aa0d993597439cbffc60c2163c491dca72816
keik <keik@c-fo.com>
2018-10-30 08:55:31 +0900

    :green_heart: Update snapshots

--- jest.config.js
+++ jest.config.js
@@ -71,7 +71,9 @@ module.exports = {
   // ],
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '\\.(css|scss)$': '<rootDir>/__mocks__/fileMock.js'
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
