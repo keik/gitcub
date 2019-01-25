9681a275fa7582f724680114019492c8ce409eea
keik <keik@c-fo.com>
2019-01-25 10:45:25 +0900

    :+1: Initialize sequelize

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: 'root',
+    password: null,
+    database: 'gh_development',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'gh_test',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'gh_production',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  }
+}
