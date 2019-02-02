dd864f293b32c88f7277181ab692bd102ab1b5bb
keik <keik@c-fo.com>
2019-02-03 02:45:53 +0900

    :+1: Apply jest-styled-components for improving snapshot testing experience

--- frontend/components/Storyshots.test.js
+++ frontend/components/Storyshots.test.js
@@ -1,6 +1,8 @@
 // @flow
 
 import initStoryshots from '@storybook/addon-storyshots'
+import 'jest-styled-components'
+
 const registerRequireContextHook = require('babel-plugin-require-context-hook/register')
 
 registerRequireContextHook()
