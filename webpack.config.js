3ff5b57e68e09c60dbfeace6c2f7e11bbe7cccb2
keik <keik@c-fo.com>
2019-02-23 22:05:57 +0900

    :shower: Remove unnecessary configurations.

--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,7 @@ module.exports = {
   },
   devServer: {
     headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-      'Access-Control-Allow-Headers':
-        'X-Requested-With, content-type, Authorization'
+      'Access-Control-Allow-Origin': '*'
     }
   },
   devtool: 'eval-source-map'
