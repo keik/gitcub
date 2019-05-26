2abb422865665e365fc5dcb082483ea4427dbbb9
keik <keik@c-fo.com>
2019-05-26 22:35:02 +0900

    :recycle: Add stories.

--- frontend/components/common/blocks/Panel.js
+++ frontend/components/common/blocks/Panel.js
@@ -1,16 +1,16 @@
 // @flow
 
+import css from '@styled-system/css'
 import * as React from 'react'
-import { css } from 'styled-components'
 
 const Panel = (p: { children: React.Node }) => (
   <div
-    css={css`
-      position: relative;
-      border: 1px solid #d8d8d8;
-      border-radius: 3px;
-      margin-bottom: 24px;
-    `}
+    css={css({
+      position: 'relative',
+      border: '1px solid #d8d8d8',
+      borderRadius: '3px',
+      mb: 3
+    })}
   >
     {p.children}
   </div>
@@ -18,12 +18,12 @@ const Panel = (p: { children: React.Node }) => (
 
 const Header = (p: { children: React.Node, info?: true, noPadding?: true }) => (
   <div
-    css={css`
-      padding: ${p.noPadding ? 0 : '12px'};
-      background-color: ${p.info ? '#f2f9fc' : '#f7f7f7'};
-      border-bottom: 1px solid;
-      border-color: ${p.info ? '#c9e6f2' : '#d8d8d8'};
-    `}
+    css={css({
+      p: p.noPadding ? 0 : 2,
+      backgroundColor: p.info ? '#f2f9fc' : '#f7f7f7',
+      borderBottom: '1px solid',
+      borderColor: p.info ? '#c9e6f2' : '#d8d8d8'
+    })}
   >
     {p.children}
   </div>
@@ -32,27 +32,28 @@ Panel.Header = Header
 
 const Body = (p: { children: React.Node, noPadding?: true }) => (
   <div
-    css={css`
-      padding: ${p.noPadding ? 0 : '12px'};
-      background-color: #fff;
-      > table {
-        width: 100%;
-        border-collapse: collapse;
-        line-height: 20px;
-        > tbody {
-          > tr {
-            &:first-of-type td {
-              border: none;
-            }
-            > td {
-              padding: 6px 3px;
-              border-top: 1px solid #eee;
-              color: #888;
+    css={css({
+      p: p.noPadding ? 0 : 2,
+      backgroundColor: '#fff',
+      '> table': {
+        width: '100%',
+        borderCollapse: 'collapse',
+        lineHeight: '20px',
+        '> tbody': {
+          '> tr': {
+            '&:first-of-type td': {
+              border: 'none'
+            },
+            '> td': {
+              py: 2,
+              px: 1,
+              borderTop: '1px solid #eee',
+              color: '#888'
             }
           }
         }
       }
-    `}
+    })}
   >
     {p.children}
   </div>
