14a78500a944b86d0483835afc889e485e3d4267
keik <keik@c-fo.com>
2019-06-08 22:43:24 +0900

    :+1: Add docker env.

--- config/config.js
+++ config/config.js
@@ -3,21 +3,21 @@
 module.exports = {
   development: {
     username: 'root',
-    password: null,
+    password: 'mysql',
     database: 'gh_development',
     host: '127.0.0.1',
     dialect: 'mysql'
   },
   test: {
     username: 'root',
-    password: null,
+    password: 'mysql',
     database: 'gh_test',
     host: '127.0.0.1',
     dialect: 'mysql'
   },
   production: {
     username: 'root',
-    password: null,
+    password: 'mysql',
     database: 'gh_production',
     host: '127.0.0.1',
     dialect: 'mysql'
